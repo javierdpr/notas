96e29029e49a5bde1589239458d9dfc26d290bb6
javierdpr <javierdpr65@gmail.com>
2024-05-16 11:06:44 +0200

    v 1.0

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+        const request = axios.get(baseUrl)
+        return request.then(response => response.data)
+    }
 
 const create = newObject => {
     const request = axios.post(baseUrl, newObject)
